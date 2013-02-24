c3f551dcd637511c7e6695bd570b68de7e90eeef
Yumer Yumer <y.yumer@gmail.com>
2013-02-24 12:39:06 +0200

    Solved problem 5.

---
+++ 02-Defining-classes-II/05. GenericClass/GenericList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+
+class GenericList<T>
+{
+    private T[] items;
+    private int currentCount = 0;
+
+    public GenericList(int capacity)
+    {
+        items = new T[capacity];
+    }
+    public int Count
+    {
+        get
+        {
+            return currentCount;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (currentCount >= items.Length)
+        {
+            throw new IndexOutOfRangeException(String.Format("Capacity of {0} items waas exceeded!", currentCount));
+        }
+        else
+        {
+            this.items[currentCount] = item;
+            currentCount++;
+        }
+    }
+
+
+    // GenericList<T> myList = new GenericList<int>();
+    // myList[5] <-- тук се реализира именно тази функционалност !!!
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= items.Length - 1)
+            {
+                throw new IndexOutOfRangeException(String.Format("Invalid index {0}", index));
+            }
+
+            T result = items[index];
+            return result;
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        if ((index < 0) || (index >= items.Length))
+        {
+            throw new IndexOutOfRangeException(String.Format("Invalid index {0}", index));
+        }
+
+        Array.Copy(this.items, index + 1, this.items, index, currentCount - index);
+        currentCount--;
+    }
+
+    public void InsertAt(int index, T value)
+    {
+        if (currentCount >= this.items.Length)
+        {
+            throw new IndexOutOfRangeException("List is full!");
+        }
+
+        Array.Copy(this.items, index, this.items, index + 1, currentCount - index + 1);
+        this.items[index] = value;
+        currentCount++;
+    }
+
+    public void Clear()
+    {
+        if (currentCount > 0)
+        {
+            Array.Clear(this.items, 0, currentCount);
+            currentCount = 0;
+        }
+    }
+
+    public int IndexOf(T item)
+    {
+        return Array.IndexOf(this.items, item);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < currentCount; i++)
+        {
+            builder.Append(this.items[i] + ", ");
+        }
+        builder.Remove(builder.Length - 2, 2);
+
+        return builder.ToString();
+    }
+
+}
---
+++ 02-Defining-classes-II/05. GenericClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        GenericList<double> gl = new GenericList<double>(5);
+        gl.Add(2.3);
+        gl.Add(5.4);
+        gl.Add(6.5);
+        gl.Add(7.5);
+        gl.RemoveAt(1);
+        gl.InsertAt(1, 5.9);
+        //gl.Clear();
+        Console.WriteLine(gl);
+    }
+}
+
+
+
+
+
