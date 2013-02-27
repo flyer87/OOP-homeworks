2f64799e2116896516052bd5b66f6264b0be642b
Yumer Yumer <y.yumer@gmail.com>
2013-02-27 23:19:34 +0200

    OOP - Extension functions, ..., LINQ - solution created

---
+++ 03. Extension-Methods-Delegates-Lambda-LINQ-Demos/TestDel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar =  new { Color = "Red", Brand = "BMW", Speed = 180 };
+            Console.WriteLine("My car is a {0} {1}.",
+              myCar.Color, myCar.Brand);
+
+        }
+    }
+}
